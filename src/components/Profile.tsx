136c403019972fc38db7988d90855f884999d32d
Taisei <taisei.960420@gmail.com>
2024-10-21 07:54:31 +0900

    修正

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Profile: React.FC = () => {
   return (
     <section>
-      <h2>名前: 山田 太郎</h2>
+      <h2>名前: テスト</h2>
       <p>自己紹介: フロントエンド開発が好きです。</p>
       <p>趣味: プログラミング、読書、ゲーム</p>
     </section>
