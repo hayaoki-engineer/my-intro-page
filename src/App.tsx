2cbfcd81c72d4e3eb98e7911dd3f72724282db57
Taisei <taisei.960420@gmail.com>
2024-10-21 07:45:41 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./components/Header";
+import Profile from "./components/Profile";
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <Header />
+      <Profile />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header: React.FC = () => {
+  return (
+    <header>
+      <h1>自己紹介ページ</h1>
+    </header>
+  );
+};
+
+export default Header;
